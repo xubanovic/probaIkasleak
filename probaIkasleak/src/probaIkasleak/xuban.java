d77f062fd48e21b9dccd1bd52457803d8367c002
Xubanovic <xubanovic@gmail.com>
2025-02-28 09:57:16 +0100

    mi segundo commit

--- probaIkasleak/src/probaIkasleak/xuban.java
+++ probaIkasleak/src/probaIkasleak/xuban.java
@@ -5,6 +5,7 @@ public class xuban {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xuban naiz");
+		System.out.println("Xuban naiz berriz");
 
 	}
 
